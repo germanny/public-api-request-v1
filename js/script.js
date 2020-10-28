f461088eb848f0680192c9ab47782d3f71c94d0d
germanny <germanny@gmail.com>
2020-10-28 18:11:09 -0500

    code refactor

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ async function fetchAllData() {
   return await fetch('https://randomuser.me/api/?results=12&nat=us,dk,fr,gb&noinfo')
     .then(checkStatus)
     .then(response => response.json())
-    .catch(error => console.log('Problem:', error));
+    .catch(error => console.log('There was a problem!', error))
 }
 
 // ADD ELEMENTS ⬇
@@ -61,12 +61,13 @@ function addCards(data) {
  * @return {string} HTML for Modal
  */
 function generateModal(person) {
-  var date = new Date(person.dob.date);
-  var month = (date.getMonth() + 1);
-  month = month < 10 ? '0' + month : month;
-  var birthdate = `${month}/${date.getDate()}/${date.getFullYear()}`;
+  const modal = document.getElementById('modal');
 
-  return `<div class="modal-container" data-uuid="${person.login.uuid}">
+  const date = new Date(person.dob.date);
+  let month = (date.getMonth() + 1);
+  month = month < 10 ? '0' + month : month;
+  const birthdate = `${month}/${date.getDate()}/${date.getFullYear()}`;
+  const modalContent = `<div class="modal-container" data-uuid="${person.login.uuid}">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
@@ -86,6 +87,9 @@ function generateModal(person) {
             <button type="button" id="modal-next" class="modal-next btn">Next</button>
         </div>
     </div>`;
+
+  modal.textContent = '';
+  modal.insertAdjacentHTML('beforeend', modalContent);
 }
 
 /**
@@ -123,7 +127,7 @@ function getCard(e) {
 
 // REMOVE ELEMENTS ⬇
 /**
- * Generate Search Form
+ * Remove modal when clicking the modal-close-btn or when paging through cards via the modal
  * @param {object} e - click event
  */
 function removeModal(e) {
@@ -151,6 +155,7 @@ function removeModal(e) {
   }
 }
 
+// ACTIONS ⬇
 /**
  * Search for value in list of students.
  * @param {object} searchInput - Input field with the value to search for
@@ -173,16 +178,37 @@ function searchList(searchInput, data) {
   }
 }
 
-// EVENTS ⬇
 /**
  * Modal Pagination
- * @param {object} e - click event
- * @param {object} data - all response data
+ * @param {object} btn - button clicked
+ * @param {array} cards - Array of cards in the gallery
+ * @param {integer} currentCardIndex - index of clicked card
+ * @param {array} list - Array of student data we are working with
  */
-function modalPagination(e, data) {
+function modalPagination(btn, cards, currentCardIndex, list) {
+  const increment = btn.classList.contains('modal-next') ? 1 : -1;
 
+  // enable or disable the button
+  if (
+    (btn.classList.contains('modal-next') && currentCardIndex > 10) ||
+    (btn.classList.contains('modal-prev') && currentCardIndex < 1)
+  ) {
+    btn.disabled = 'disabled';
+  } else {
+    btn.disabled = '';
+
+    // remove current modal
+    document.addEventListener('click', removeModal);
+
+    // move to next or prev card
+    const person = list.find(item => item.login.uuid === cards[currentCardIndex + increment].getAttribute('data-uuid'));
+
+    // make a new modal
+    generateModal(person);
+  }
 }
 
+// EVENTS ⬇
 // After the DOM loads, fetch data for cards and display
 document.addEventListener('DOMContentLoaded', async () => {
   // Add search form
@@ -209,8 +235,11 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 
   // Pop open a modal on card click
+  const gallery = document.getElementById('gallery');
+
   gallery.addEventListener('click', (e) => {
     const card = getCard(e);
+
     if (!card) {
       return;
     }
@@ -218,60 +247,37 @@ document.addEventListener('DOMContentLoaded', async () => {
     const uuid = card.getAttribute('data-uuid');
     const person = items.results.find(item => item.login.uuid === uuid);
 
-    gallery.insertAdjacentHTML('afterend', generateModal(person));
+    generateModal(person);
   });
 
   // Modal pagination click events
-  document.addEventListener('click', (e) => {
+  const modal = document.getElementById('modal');
+
+  modal.addEventListener('click', (e) => {
     if (e.target.tagName !== 'BUTTON') {
       return;
     }
 
     // get the button type
     const btn = e.target;
-    const btnClass = e.target.getAttribute('id');
 
-    // get all the cards
-    const cards = [...document.querySelectorAll('.card')];
+    if (btn.classList.contains('btn')) {
+      // get all the cards
+      const cards = [...document.querySelectorAll('.card')];
 
-    // get the uuid from the modal
-    const modalUuid = e.target.parentNode.parentNode.getAttribute('data-uuid');
+      // get the uuid from the modal
+      const modalUuid = e.target.parentNode.parentNode.getAttribute('data-uuid');
 
-    // look for the card with the matching uuid
-    const currentCard = cards.find(card => card.getAttribute('data-uuid') === modalUuid);
-    const currentCardIndex = cards.indexOf(currentCard);
-    console.log(currentCardIndex);
-    console.log(btnClass);
+      // look for the card with the matching uuid
+      const currentCard = cards.find(card => card.getAttribute('data-uuid') === modalUuid);
+      const currentCardIndex = cards.indexOf(currentCard);
 
-    // get the next or prev item, depending on the button type
-    // display new modal
-
-    if (btn.classList.contains('modal-next')) {
-      if (currentCardIndex > 10) {
-        btn.disabled = true;
-        console.log('STOP');
-      } else {
-        console.log(e.target);
-
-        // remove current modal
-        document.addEventListener('click', removeModal);
-
-        const person = items.results.find(item => item.login.uuid === cards[currentCardIndex + 1].getAttribute('data-uuid'));
-        gallery.insertAdjacentHTML('afterend', generateModal(person));
-      }
+      // get the next or prev item, depending on the button type
+      // display new modal
+      modalPagination(btn, cards, currentCardIndex, items.results);
     }
 
-    if (btn.classList.contains('modal-prev') && currentCardIndex !== 1) {
-
-      // remove current modal
-      document.addEventListener('click', removeModal);
-
-      const person = items.results.find(item => item.login.uuid === cards[currentCardIndex - 1].getAttribute('data-uuid'));
-      gallery.insertAdjacentHTML('afterend', generateModal(person));
-
-    }
+    removeModal(e);
   });
-
 });
 
-document.addEventListener('click', removeModal);
